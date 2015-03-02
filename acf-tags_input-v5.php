c3c8610aca9f80555ded9039f27cab25f0cd45a0
kolhoffmmm <kolhoffmmm@gmail.com>
2015-03-02 22:18:05 +0200

    Create acf-tags_input-v5.php

---
+++ acf-tags_input-v5.php
@@ -0,0 +1,511 @@
+<?php
+
+class acf_field_tags_input extends acf_field {
+	
+	
+	/*
+	*  __construct
+	*
+	*  This function will setup the field type data
+	*
+	*  @type	function
+	*  @date	5/03/2014
+	*  @since	5.0.0
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+	
+	function __construct() {
+		
+		// vars
+		$this->name = 'tag';
+		$this->label = __("Tag",'acf');
+		$this->defaults = array(
+			'tagtax'		=> '',
+		);
+		
+				
+		// do not delete!
+    	parent::__construct();
+    	
+	}
+	
+	
+	/*
+	*  render_field_settings()
+	*
+	*  Create extra settings for your field. These are visible when editing a field
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field (array) the $field being edited
+	*  @return	n/a
+	*/
+	
+	function render_field_settings( $field ) {
+			
+		// Tags Taxonmoy
+
+		acf_render_field_setting( $field, array(
+			'label'			=> __('Tag Taxonomy','acf'),
+			'type'			=> 'select',
+			'name'			=> 'tagtax',
+			'choices'		=> acf_get_taxonomies(),
+		));		
+	}
+	
+	
+	
+	/*
+	*  render_field()
+	*
+	*  Create the HTML interface for your field
+	*
+	*  @param	$field (array) the $field being rendered
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field (array) the $field being edited
+	*  @return	n/a
+	*/
+	
+	function render_field( $field ) {
+		
+		
+		/*
+		*  Review the data of $field.
+		*  This will show what data is available
+		*/
+		
+		//echo '<pre>';
+		//	print_r($field);
+		//echo '</pre>';
+		
+		
+		/*
+		*  Create a simple text input using the 'font_size' setting.
+		*/
+		//print_r($field);
+		$value=esc_attr($field['value']);
+		if(is_admin()){$type="readonly"; $value="";}
+		?>
+		<input type="text" name="<?php echo esc_attr($field['name']) ?>" value="<?php echo $value ?>" style="font-size:<?php echo $field['font_size'] ?>px;" <?php echo $type;?>/>
+		<?php
+	}
+	
+		
+	/*
+	*  input_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is created.
+	*  Use this action to add CSS + JavaScript to assist your render_field() action.
+	*
+	*  @type	action (admin_enqueue_scripts)
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+
+	/*
+	
+	function input_admin_enqueue_scripts() {
+		
+		$dir = plugin_dir_url( __FILE__ );
+		
+		
+		// register & include JS
+		wp_register_script( 'acf-input-tags_input', "{$dir}js/input.js" );
+		wp_enqueue_script('acf-input-tags_input');
+		
+		
+		// register & include CSS
+		wp_register_style( 'acf-input-tags_input', "{$dir}css/input.css" ); 
+		wp_enqueue_style('acf-input-tags_input');
+		
+		
+	}
+	
+	*/
+	
+	
+	/*
+	*  input_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is created.
+	*  Use this action to add CSS and JavaScript to assist your render_field() action.
+	*
+	*  @type	action (admin_head)
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+
+	/*
+		
+	function input_admin_head() {
+	
+		
+		
+	}
+	
+	*/
+	
+	
+	/*
+   	*  input_form_data()
+   	*
+   	*  This function is called once on the 'input' page between the head and footer
+   	*  There are 2 situations where ACF did not load during the 'acf/input_admin_enqueue_scripts' and 
+   	*  'acf/input_admin_head' actions because ACF did not know it was going to be used. These situations are
+   	*  seen on comments / user edit forms on the front end. This function will always be called, and includes
+   	*  $args that related to the current screen such as $args['post_id']
+   	*
+   	*  @type	function
+   	*  @date	6/03/2014
+   	*  @since	5.0.0
+   	*
+   	*  @param	$args (array)
+   	*  @return	n/a
+   	*/
+   	
+   	/*
+   	
+   	function input_form_data( $args ) {
+	   	
+		
+	
+   	}
+   	
+   	*/
+	
+	
+	/*
+	*  input_admin_footer()
+	*
+	*  This action is called in the admin_footer action on the edit screen where your field is created.
+	*  Use this action to add CSS and JavaScript to assist your render_field() action.
+	*
+	*  @type	action (admin_footer)
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+
+	/*
+		
+	function input_admin_footer() {
+	
+		
+		
+	}
+	
+	*/
+	
+	
+	/*
+	*  field_group_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is edited.
+	*  Use this action to add CSS + JavaScript to assist your render_field_options() action.
+	*
+	*  @type	action (admin_enqueue_scripts)
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+
+	/*
+	
+	function field_group_admin_enqueue_scripts() {
+		
+	}
+	
+	*/
+
+	
+	/*
+	*  field_group_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is edited.
+	*  Use this action to add CSS and JavaScript to assist your render_field_options() action.
+	*
+	*  @type	action (admin_head)
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	n/a
+	*  @return	n/a
+	*/
+
+	/*
+	
+	function field_group_admin_head() {
+	
+	}
+	
+	*/
+
+
+	/*
+	*  load_value()
+	*
+	*  This filter is applied to the $value after it is loaded from the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value (mixed) the value found in the database
+	*  @param	$post_id (mixed) the $post_id from which the value was loaded
+	*  @param	$field (array) the field array holding all the field options
+	*  @return	$value
+	*/
+	
+	
+	
+	function load_value( $value, $post_id, $field ) {
+		if(is_admin()){return;}
+		$tax=$field['tagtax'];
+		$terms=array();
+		$value= ''; 
+		$terms = get_the_terms( $post_id, $tax );
+		$count = count($terms);
+		if($count > 1) {
+		$i = 0;
+		foreach($terms as $term) {
+		if(++$i != $count) {
+		$value.=$term->name.',';
+		}
+		else {$value.=$term->name;}}}  
+		return $value;
+		
+	}
+	
+	
+	
+	/*
+	*  update_value()
+	*
+	*  This filter is applied to the $value before it is saved in the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value (mixed) the value found in the database
+	*  @param	$post_id (mixed) the $post_id from which the value was loaded
+	*  @param	$field (array) the field array holding all the field options
+	*  @return	$value
+	*/
+	
+	
+	
+	function update_value( $value, $post_id, $field ) {
+		if(is_admin()){return;}
+		$tax=$field['tagtax'];
+		$tags = explode(",",strip_tags(trim($value)));
+		$tagsReturn = array(0);
+		for ($j=0;$j!=count($tags); $j++)
+		array_push($tagsReturn,$tags[$j]);
+		wp_set_object_terms( $post_id, $tagsReturn, $tax);
+		//return $value;
+		return "";
+	}
+	
+	
+	
+	
+	/*
+	*  format_value()
+	*
+	*  This filter is applied to the $value after it is loaded from the db and before it is passed to the render_field() function
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value (mixed) the value which was loaded from the database
+	*  @param	$post_id (mixed) the $post_id from which the value was loaded
+	*  @param	$field (array) the field array holding all the field options
+	*  @param	$template (boolean) true if value requires formatting for front end template function
+	*  @return	$value
+	*/
+		
+	/*
+	
+	function format_value( $value, $post_id, $field, $template ) {
+		
+		// bail early if not template function such as get_field()
+		if( !$template )
+		{
+			return $value
+		}
+		
+		return $value;
+	}
+	
+	*/
+	
+	
+	/*
+	*  validate_value()
+	*
+	*  This filter is used to perform validation on the value prior to saving.
+	*  All values are validated regardless of the field's required setting. This allows you to validate and return
+	*  messages to the user if the value is not correct
+	*
+	*  @type	filter
+	*  @date	11/02/2014
+	*  @since	5.0.0
+	*
+	*  @param	$valid (boolean) validation status based on the value and the field's required setting
+	*  @param	$value (mixed) the $_POST value
+	*  @param	$field (array) the field array holding all the field options
+	*  @param	$input (string) the corresponding input name for $_POST value
+	*  @return	$valid
+	*/
+	
+	/*
+	
+	function validate_value( $valid, $value, $field, $input ){
+		
+		// Basic usage
+		if( $value < $field['custom_minimum_setting'] )
+		{
+			$valid = false;
+		}
+		
+		
+		// Advanced usage
+		if( $value < $field['custom_minimum_setting'] )
+		{
+			$valid = __('The value is too little!','acf-tags_input'),
+		}
+		
+		
+		// return
+		return $valid;
+		
+	}
+	
+	*/
+	
+	
+	/*
+	*  delete_value()
+	*
+	*  This action is fired after a value has been deleted from the db.
+	*  Please note that saving a blank value is treated as an update, not a delete
+	*
+	*  @type	action
+	*  @date	6/03/2014
+	*  @since	5.0.0
+	*
+	*  @param	$post_id (mixed) the $post_id from which the value was deleted
+	*  @param	$key (string) the $meta_key which the value was deleted
+	*  @return	n/a
+	*/
+	
+	/*
+	
+	function delete_value( $post_id, $key ) {
+		
+		
+		
+	}
+	
+	*/
+	
+	
+	/*
+	*  load_field()
+	*
+	*  This filter is applied to the $field after it is loaded from the database
+	*
+	*  @type	filter
+	*  @date	23/01/2013
+	*  @since	3.6.0	
+	*
+	*  @param	$field (array) the field array holding all the field options
+	*  @return	$field
+	*/
+	
+	/*
+	
+	function load_field( $field ) {
+		
+		return $field;
+		
+	}	
+	
+	*/
+	
+	
+	/*
+	*  update_field()
+	*
+	*  This filter is applied to the $field before it is saved to the database
+	*
+	*  @type	filter
+	*  @date	23/01/2013
+	*  @since	3.6.0
+	*
+	*  @param	$field (array) the field array holding all the field options
+	*  @return	$field
+	*/
+	
+	/*
+	
+	function update_field( $field ) {
+		
+		return $field;
+		
+	}	
+	
+	*/
+	
+	
+	/*
+	*  delete_field()
+	*
+	*  This action is fired after a field is deleted from the database
+	*
+	*  @type	action
+	*  @date	11/02/2014
+	*  @since	5.0.0
+	*
+	*  @param	$field (array) the field array holding all the field options
+	*  @return	n/a
+	*/
+	
+	/*
+	
+	function delete_field( $field ) {
+		
+		
+		
+	}	
+	
+	*/
+	
+	
+}
+
+
+// create field
+new acf_field_tags_input();
+
+?>
